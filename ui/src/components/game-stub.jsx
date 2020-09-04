887c96b1a94224ed721d27edcfc78a88276d6042
infernezor@gmail.com <infernezor@gmail.com>
2020-09-04 16:07:33 -0400

    refactoring some code.

--- ui/src/components/game-stub.jsx
+++ ui/src/components/game-stub.jsx
@@ -84,11 +84,8 @@ function GameStub() {
       let test = [];
       if (UserAchievements !== undefined) {
         UserAchievements.forEach((game) => {
-          console.log('we are peeking at User Achivement Game: ', game);
           if (game.game_no === currentGame.id) {
-            console.log('we have a match.');
             test.push(game);
-            console.log('the match is: ', game);
             return game;
           }
         });
@@ -139,76 +136,78 @@ function GameStub() {
   };
 
   const RenderTest = (props) => {
-    // const playerHasAchievement = CurrentGameAchievement.find((x) => {
-    //   return x.id === props.achievement.id;
-    // });
+    const playerHasAchievement = CurrentGameAchievement.find((x) => {
+      return x.achievement_no === props.achievement.id;
+    });
+    console.log('player has, ', playerHasAchievement);
+
+    let display;
     for (const a of CurrentGameAchievement) {
-      if (a.id === props.achievement.id) {
-        //return button
+      if (a.achievement_no === props.achievement.id) {
       }
     }
     //return other button.
 
     //get current achivements claimed in this game by the user.
-    console.log('dfosdif', CurrentGameAchievement.length);
-    console.log(
-      CurrentGameAchievement.length,
-      SelectedGame.achievements.length,
-    );
-    if (CurrentGameAchievement.length === SelectedGame.achievements.length) {
-      console.log('You have done it all!');
-      return (
-        <h1>
-          You've completed them all! please install an remove achievement
-          button.
-        </h1>
-      );
-    }
-    if (CurrentGameAchievement.length === 0) {
-      let display = SelectedGame.achievements.map((achieve) => {
-        if (achieve.id !== props.achievement.id) {
-          console.log('hello!', props.achievement.id, achieve.achievement_no);
-          return (
-            <Button
-              className='ui toggle button'
-              aria-pressed='false'
-              onClick={() => {
-                claimAchievement(props.achievement);
-              }}
-            >
-              Claim Achievement
-            </Button>
-          );
-        }
-      });
-      return display;
-    }
-    if (CurrentGameAchievement.length >= 1) {
-      let display = CurrentGameAchievement.map((achieve) => {
-        console.log('primary achieve at the base is: ', achieve);
+    // console.log('dfosdif', CurrentGameAchievement.length);
+    // console.log(
+    //   CurrentGameAchievement.length,
+    //   SelectedGame.achievements.length,
+    // );
+    // if (CurrentGameAchievement.length === SelectedGame.achievements.length) {
+    //   console.log('You have done it all!');
+    //   return (
+    //     <h1>
+    //       You've completed them all! please install an remove achievement
+    //       button.
+    //     </h1>
+    //   );
+    // }
+    // if (CurrentGameAchievement.length === 0) {
+    //   let display = SelectedGame.achievements.map((achieve) => {
+    //     if (achieve.id !== props.achievement.id) {
+    //       console.log('hello!', props.achievement.id, achieve.achievement_no);
+    //       return (
+    //         <Button
+    //           className='ui toggle button'
+    //           aria-pressed='false'
+    //           onClick={() => {
+    //             claimAchievement(props.achievement);
+    //           }}
+    //         >
+    //           Claim Achievement
+    //         </Button>
+    //       );
+    //     }
+    //   });
+    //   return display;
+    // }
+    // if (CurrentGameAchievement.length >= 1) {
+    //   let display = CurrentGameAchievement.map((achieve) => {
+    //     console.log('primary achieve at the base is: ', achieve);
 
-        if (achieve.achievement_no !== props.achievement.id) {
-          console.log('hello!', props.achievement.id);
-          return (
-            <Button
-              // variant='primary'
-              className='ui toggle button'
-              aria-pressed='false'
-              onClick={() => {
-                claimAchievement(props.achievement);
-              }}
-            >
-              Claim Achievement
-            </Button>
-          );
-        } else {
-          return <h1> already claimed</h1>;
-        }
-      });
+    //     if (achieve.achievement_no !== props.achievement.id) {
+    //       console.log('hello!', props.achievement.id);
+    //       return (
+    //         <Button
+    //           // variant='primary'
+    //           className='ui toggle button'
+    //           aria-pressed='false'
+    //           onClick={() => {
+    //             claimAchievement(props.achievement);
+    //           }}
+    //         >
+    //           Claim Achievement
+    //         </Button>
+    //       );
+    //     } else {
+    //       return <h1> already claimed</h1>;
+    //     }
+    //   });
 
-      console.log(CurrentGameAchievement);
-      return display;
-    }
+    console.log('current game achievements', CurrentGameAchievement);
+    console.log('display ', display);
+    return null;
   };
 
   return (
